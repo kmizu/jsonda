f02c4d105e154c56408ffada2e2226b614dbe7ed
Kota Mizushima <mizukota@gmail.com>
2012-05-25 16:27:15 +0900

    Add scaladoc documentation for JsondaParser

--- src/main/scala/org/onion_lang/jsonda/parser/JsondaParser.scala
+++ src/main/scala/org/onion_lang/jsonda/parser/JsondaParser.scala
@@ -1,5 +1,12 @@
 package org.onion_lang.jsonda.parser
 
+/** == Overview ==
+ * I have the plan to yet another JSON Parser which behavior is customized
+ * easily.  For example, I think that it is better to provide the mechanism
+ * that users can register custom handlers.  If there exists,  library users
+ * can interpret each JSON data as you like.
+ * However, the implementation is not started yet.
+ */
 class JsondaParser
 
 object JsondaParser {
